16048fe69c94fbbf8113ac3d28368daac542dd79
Ayanda Sangweni <asangweni51@gmail.com>
2024-12-01 15:29:26 +0200

    Fixed genremapping

--- my-app/src/utils/genreMap.js
+++ my-app/src/utils/genreMap.js
@@ -1,11 +1,11 @@
 export const genreMap = {
-    1: 'Health & Wellness',
-    2: 'True Crime',
+    1: 'Personal Growth',
+    2: 'Investigative Journalism',
     3: 'History',
-    4: 'Biography',
-    5: 'Comedy',
+    4: 'Comedy',
+    5: 'Entertainment',
     6: 'Business',
     7: 'Fiction',
-    8: 'True Crime',
-    9: 'Adventure'
-  };
+    8: 'News',
+    9: 'Kids and Family'
+  };  
